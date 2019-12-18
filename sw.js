9d85b8a30ca0117db788430ae3f31e0c05d3bf3c
stan smolyar <s.light.post@gmail.com>
2019-12-18 13:30:15 +0700

    manifest for gh pages

--- sw.js
+++ sw.js
@@ -23,7 +23,7 @@ self.addEventListener('install', function(event) {
 
 self.addEventListener('fetch', function(event) {
   event.respondWith(
-    caches.match(event.request)
+    caches.match(event.request,{ignoreSearch:true})
       .then(function(response) {
         // Cache hit - return response
         if (response) {
